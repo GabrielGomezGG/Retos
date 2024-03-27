653def5dc7708f0c6d651230b46114d881575f8e
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-03-27 13:05:59 -0300

    Reto 1 Completado

---
+++ Reto 1/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val text1 = "dabale arroz a la zorra el abad"
+    println("¿La palabra '$text1' es palindrome? : ${isPalindrome(text1)}")
+
+    val text2 = "anita lava la tina"
+    println("¿La palabra '$text2' es palindrome? : ${isPalindrome(text2)}")
+}
+
+fun isPalindrome(text: String):Boolean{
+    // Elimino todos los espacios del string
+    val textWithoutSpaces = text.replace(" ", "")
+
+    // Invierto el string sin espacios
+    val reversedText = textWithoutSpaces.reversed()
+
+    // Comparo si el string sin espacios es igual al string invertido
+    return textWithoutSpaces == reversedText
+}
